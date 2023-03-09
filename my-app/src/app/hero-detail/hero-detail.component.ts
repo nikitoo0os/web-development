26d5af416f7423d69ad7647ccdf150855172141b
horse18leet <gluhih20003@mail.ru>
2023-03-09 16:33:33 +0300

    123

---
+++ my-app/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+
+  goBack(): void {
+    this.location.back();
+  }
+  save(): void{
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
+}
